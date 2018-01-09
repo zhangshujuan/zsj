724272d317ae3f25a8891fe5b74f74614d531664
zhangshujuan <289784710@qq.com>
2018-01-09 17:26:20 +0800

    update

--- js/luoym.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author luoym
- * @email 13575458746@163.com
- * @descrip luoym.com
- * @version v1.0.0
- */
-$(function(){var n={init:function(){n.bind()},bind:function(){n.headerNavButtonBind()},headerNavButtonBind:function(){var n=$(".header .btn-nav");n.on("click",function(){var n=$(this);n.toggleClass("btn-nav-active"),$(".navbar").toggleClass("navbar-active"),$("body").toggleClass("body-trans-left")})}};n.init()});
